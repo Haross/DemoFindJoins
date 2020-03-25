29b297264dab8a451b0898c9b18cc8c58e423520
Javier Flores <javierfh1404@gmail.com>
2020-03-25 01:09:26 +0100

    jupyter code

--- src/com/javier/features/DistributedSparkTest.scala
+++ src/com/javier/features/DistributedSparkTest.scala
@@ -1,5 +1,33 @@
 package com.javier.features
 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col,when}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+
 object DistributedSparkTest {
 
+  def main(args: Array[String]): Unit = {
+
+    val sparkUrl = "192.168.99.119:7077"
+
+    val spark = SparkSession.builder.appName("testDs")
+      .master(s"spark://$sparkUrl")
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
+
+    val schemaCVS = StructType(Array(
+      StructField("cast", StringType, true),
+      StructField("crew", StringType, true),
+      StructField("id", IntegerType, true)
+    ))
+
+
+    val path = "/Users/javierflores/filesSpark/"
+    val df = spark.read.option("header", "true").schema(schemaCVS).csv(s"${path}credits.csv")
+
+    df.printSchema
+    df.filter(col("id").isNull).select(col("id")*2).show(10000)
+  }
 }
